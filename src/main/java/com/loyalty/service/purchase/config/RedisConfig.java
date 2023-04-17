972811253402cf2e50e621720159c2277057913f
ankitdabhi <ankit.dabhi.ext@seamless.se>
2023-04-17 08:40:08 +0530

    code changes pushed

--- src/main/java/com/loyalty/service/purchase/config/RedisConfig.java
+++ src/main/java/com/loyalty/service/purchase/config/RedisConfig.java
@@ -1,20 +1,40 @@
 package com.loyalty.service.purchase.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 @EnableRedisRepositories
-public class RedisConfig<T> {
+public class RedisConfig {
 
-    @Bean(name = "redisTemplate")
-    public RedisTemplate<String, T> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
-        RedisTemplate<String, T> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory);
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisProperties properties = redisProperties();
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(properties.getHost());
+        configuration.setPort(properties.getPort());
+
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
 
+    @Bean
+    @Primary
+    public RedisProperties redisProperties() {
+        return new RedisProperties();
+    }
 }
